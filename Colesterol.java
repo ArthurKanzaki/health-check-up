f8fdfc2f56598de52e792686a55f3eb9f15146c2
Arthur Vieira <arthurvieirapedrosa@hotmail.com>
2023-08-17 01:14:30 +0000

    criando classe exame abstrata

---
+++ Colesterol.java
@@ -0,0 +1,21 @@
+public class Colesterol extends Exame{
+
+    @Override
+    public void classficarResultado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'classficarResultado'");
+    }
+
+    @Override
+    public void mostrarResultado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarResultado'");
+    }
+
+    @Override
+    public void cadastrarExame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrarExame'");
+    }
+    
+}
---
+++ Glicemia.java
@@ -0,0 +1,31 @@
+public class Glicemia extends Exame{
+
+    private int glicose;
+
+    public int getGlicose(){
+        return glicose;
+    }
+    
+    public void setGlicose(int glicose){
+        this.glicose = glicose;
+    }
+
+    @Override
+    public void classficarResultado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'classficarResultado'");
+    }
+
+    @Override
+    public void mostrarResultado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrarResultado'");
+    }
+
+    @Override
+    public void cadastrarExame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrarExame'");
+    }
+    
+}
